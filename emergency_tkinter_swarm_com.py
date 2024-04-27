469b3947ff0443b01fec1851a23c5e8ff85dbb7c
stan.goodwin6@mail.dcu.ie <stan.goodwin6@mail.dcu.ie>
2024-04-27 12:58:06 +0100

    fixing followers leaving

--- emergency_tkinter_swarm_com.py
+++ emergency_tkinter_swarm_com.py
@@ -3,6 +3,7 @@ import random
 from collections import deque
 import numpy as np
 import math
+from create_office import layout
 
 # Simulation Properties
 open_space = 0
@@ -12,65 +13,7 @@ grid_size = 100
 cell_size = 8  # Adjust the size of each cell
 center_of_mass_radius = 2  # Radius to consider for calculating local center of mass
 
-# Grid setup
-
-# Set the outside wall
-grid = [[wall if i == 0 or i == grid_size - 1 or j == 0 or j == grid_size - 1 else open_space for j in range(grid_size)] for i in range(grid_size)]
-
-# Create the 4 office walls
-for i in range(0, 46):
-    for j in [45, 55]:
-        grid[i][j] = wall
-        grid[i][j] = wall
-        grid[j][i] = wall
-        grid[j][i] = wall
-
-for i in range(55, 100):
-    for j in [45, 55]:
-        grid[i][j] = wall
-        grid[i][j] = wall
-        grid[j][i] = wall
-        grid[j][i] = wall
-
-# Create the internal doors
-grid[45][24:26] = open_space, open_space
-grid[55][24:26] = open_space, open_space
-grid[45][75:77] = open_space, open_space
-grid[55][75:77] = open_space, open_space
-
-for row in range(24, 26):
-    grid[row][45] = open_space
-    grid[row][55] = open_space
-
-for row in range(76, 78):
-    grid[row][45] = open_space
-    grid[row][55] = open_space
-
-# Create the desks
-for i in range(10, 35):
-    for j in range(7, 38, 10):
-        grid[i][j] = wall
-        grid[i][j] = wall
-
-    for j in range(63, 99, 10):
-        grid[i][j] = wall
-        grid[i][j] = wall
-
-for i in range(65, 90):
-    for j in range(7, 38, 10):
-        grid[i][j] = wall
-        grid[i][j] = wall
-
-    for j in range(63, 99, 10):
-        grid[i][j] = wall
-        grid[i][j] = wall
-
-# Set the exits
-for i in range(49, 51):
-    grid[0][i] = exit  # Top
-    grid[99][i] = exit # Bottom
-    grid[i][0] = exit  # Left
-    grid[i][99] = exit  # Right
+grid = layout()
 
 class Follower:
     def __init__(self):
@@ -83,7 +26,8 @@ class Follower:
 
     def follow_local_center_of_mass(self, leaders, followers):
         if self.move_towards_exit():
-            self.move_towards_exit()
+            self.escaped = True
+            return True
         elif self.random_movement_counter > 0:
             self.random_movement_counter -= 1
             self.random_movement()
@@ -160,10 +104,12 @@ class Follower:
             move_y = np.sign(exit_pos[1] - self.y)
             new_x = int(self.x + move_x)
             new_y = int(self.y + move_y)
-            if 0 <= new_x < grid_size and 0 <= new_y < grid_size and grid[new_x][new_y] == open_space:
+            if grid[new_x][new_y] == exit:
+                self.escaped = True
+                return True
+            elif 0 <= new_x < grid_size and 0 <= new_y < grid_size and grid[new_x][new_y] == open_space:
                 self.x = new_x
                 self.y = new_y
-                return True
         return False
 
 class FireWarden:
